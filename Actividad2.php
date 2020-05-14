a67f468a49a54a053ad8e6c70604a8123edece75
MarianaMT <marianatellez2003@gmail.com>
2020-05-14 13:02:56 -0500

    FEAT:Actividad2

---
+++ Actividad2.php
@@ -0,0 +1,4 @@
+<?php
+    echo"Melo Téllez Mariana Itzel";
+    echo"Definitivamente la parte de arreglos(aka con la que más sufro) y la parte de sesiones y cookies. Me gusta mucho que todos son amables.(:";
+?>
